b9c36d2d1a99911475ebe2edb70b77ecfe1c710c
tenor2000 <tenor2000@gmail.com>
2024-04-22 12:39:54 -0400

    Set up GH-Pages branch for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,12 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Output to the 'dist' directory
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Battleship',
     }),
   ],
   optimization: {
